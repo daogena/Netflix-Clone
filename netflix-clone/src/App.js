3a9b4c0ce7de91fd70f32ff674e9e3043f519b56
Gena <daogena@gmail.com>
2020-07-23 22:36:25 -0500

    Create rows for movies

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
+      {/* Nav */}
+
+      <Banner />
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Romance Movies" fetchURL={requests.fetchRomancesMovies}/>
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
     </div>
   );
 }
